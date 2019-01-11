dd897411b179a5bd7b0d2cd2129fb8aa8eefe7b3
Arul <arul.csecit@ymail.com>
2019-01-11 16:55:29 +0530

    fixed for python3, latest opencv

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ import lomo
 data_path = 'data'
 img_list = os.listdir(data_path)
 if len(img_list) == 0:
-    print 'Data directory is empty.'
+    print('Data directory is empty.')
     exit()
 
 with open('config.json', 'r') as f:
@@ -20,6 +20,6 @@ for img_name in img_list:
 
     img = cv2.imread(os.path.join(data_path, img_name))
 
-    lomo = lomo.LOMO(img, config)
+    lomo_desc = lomo.LOMO(img, config)
 
-    print 'Lomo feature size:', lomo.shape[0]
+    print('Lomo feature size:', lomo_desc.shape[0])
